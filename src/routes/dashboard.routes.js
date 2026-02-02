de73f03fe86a06ff729b43cb5503b27a1b4370a1
hrach347 <hrachpapyan347@gmail.com>
2026-02-02 09:47:00 +0400

    on / redirect to /dashboard

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -3,6 +3,7 @@ import { getDashboard } from "../controllers/dashboard.controller.js"
 
 const router = Router()
 
+router.get("/", (req, res) => { res.redirect("/dashboard") })
 router.get("/dashboard", getDashboard)
 
 export default router
